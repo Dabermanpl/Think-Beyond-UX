3a5c19e21b05223d18950cea5d6fbc614e81e549
Emilian Storta <dabermanpl@gmail.com>
2026-03-15 22:34:13 +0100

    feat: add Microsoft Clarity tracking script to the layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Newsreader, JetBrains_Mono } from "next/font/google";
+import Script from "next/script";
 import ScrollToTop from "@/components/ScrollToTop";
 import "./globals.css";
 
@@ -50,6 +51,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pl">
+      <head>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "vw5nwha7xw");
+          `}
+        </Script>
+      </head>
       <body
         className={`${inter.variable} ${newsreader.variable} ${jetbrainsMono.variable} antialiased`}
       >
